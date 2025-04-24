0e8069ba97b2a6b91417c6d07d76662ff9d82385
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-04-24 23:54:14 +0400

    refactor(api): replace fetch with axios for HTTP requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,42 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://dummyjson.com/products"
+})
+
 export const API = {
     getFlashSales(dispatch) {
-        fetch(`https://dummyjson.com/products/search?q=phone&limit=4`)
-            .then(res => res.json())
-            .then((res) => dispatch({type : "api_flashSales", payload : [...res.products.map((elem) => ({ ...elem, count: 1 }))]
-        }))
+        instance.get(`/search?q=phone&limit=4`)
+        .then((res) => {
+            dispatch({
+                type: "api_flashSales",
+                payload: res.data.products.map((elem) => ({
+                    ...elem,
+                    count: 1
+                }))
+            });
+        })
     },
 
     getSliderImage(dispatch) {
         Promise.all([
-            fetch(`https://dummyjson.com/products/99?select=thumbnail,title`),
-            fetch(`https://dummyjson.com/products/93?select=thumbnail,title`),
-            fetch(`https://dummyjson.com/products/79?select=thumbnail,title`),
-            fetch(`https://dummyjson.com/products/193?select=thumbnail,title`),
-          ]).then((res) => Promise.all(res.map(r => r.json())))
-            .then((res) => dispatch({type : "api_slider", payload : res
-        }))
+            instance.get(`/99?select=thumbnail,title`),
+            instance.get(`/93?select=thumbnail,title`),
+            instance.get(`/79?select=thumbnail,title`),
+            instance.get(`/193?select=thumbnail,title`),
+          ]).then(responses => {
+            const data = responses.map(response => response.data);
+            dispatch({ type: "api_slider", payload: data });
+          })
     },
 
     getProduct(dispatch, id) {
-        fetch(`https://dummyjson.com/products/${id}`)
-            .then(res => res.json())
-            .then(res => dispatch({type : "api_product", payload : { ...res, count: 1 }}));
+        instance.get(`/${id}`)
+            .then(res => dispatch({type : "api_product", payload : { ...res.data, count: 1 }}));
     },
 
     getCategory(dispatch, name) {
-        fetch(`https://dummyjson.com/products/category/${name}`)
-            .then(res => res.json())
-            .then((res) => dispatch({type : "api_category", payload : res.products}))
+        instance.get(`/category/${name}`)
+            .then((res) => dispatch({type : "api_category", payload : res.data.products}))
     }
 }
